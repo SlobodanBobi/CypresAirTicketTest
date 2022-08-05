621eb11e598a459bc2ad47286b301986f02069bb
SlobodanBobi <bozinovski.slobodan@gmail.com>
2022-08-05 09:20:56 +0200

    bobi

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,7 +1,6 @@
 // ***********************************************************
 // This example support/e2e.ts is processed and
 // loaded automatically before your test files.
-//
 // This is a great place to put global configuration and
 // behavior that modifies Cypress.
 // You can change the location of this file or turn off
@@ -10,9 +9,7 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
 // Import commands.js using ES2015 syntax:
 import './commands'
-
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
